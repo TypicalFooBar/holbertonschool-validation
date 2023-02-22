fe18e9d5c7a0eca1771ad2f6f91a84fbe066a763
Derek Webb <dwebb.af@gmail.com>
2023-02-22 17:47:01 +0000

    Task3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-apt update
-wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
-tar -C /usr/local/bin -xzf hugo_0.110.0_Linux-64bit.tar.gz
-rm ./hugo_0.110.0_Linux-64bit.tar.gz
+apt-get update
+apt-get install -y hugo make
