c8a1f044e0016d8ec00206b93134acf3794c1c28
Derek Webb <dwebb.af@gmail.com>
2023-02-22 18:02:51 +0000

    Other files

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y make golang-go curl
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+
 make build
