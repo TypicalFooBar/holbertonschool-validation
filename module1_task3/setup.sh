9afa5b1bded46e9591c3280c5d5c40ee521914b2
Derek Webb <dwebb.af@gmail.com>
2023-02-22 17:58:04 +0000

    Task3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-apt-get update && apt-get install -y hugo make
+apt-get update && apt-get install -y make wget
+
+# Download Go
+wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+rm ./go1.20.1.linux-amd64.tar.gz
+# Set the path to include Go's bin directory
+export PATH=$PATH:/usr/local/go/bin
+
+# Download the Hugo executable
+wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
+tar -C /usr/local/bin -xzf hugo_0.110.0_Linux-64bit.tar.gz
+rm ./hugo_0.110.0_Linux-64bit.tar.gz
+
 make build
