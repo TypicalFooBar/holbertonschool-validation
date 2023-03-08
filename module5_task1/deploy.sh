d212fed78469db4ecdbdd5646dec34865edef394
Derek Webb <dwebb.af@gmail.com>
2023-03-08 14:43:38 -0600

    Shellcheck changes

--- module5_task1/deploy.sh
+++ module5_task1/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Read the IP address from the file
-new_ec2_instance_public_ip=`cat new_ec2_instance_public_ip.txt`
+new_ec2_instance_public_ip=$(cat new_ec2_instance_public_ip.txt)
 
-scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@$new_ec2_instance_public_ip:/home/ubuntu/
+scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
 
-ssh -o StrictHostKeyChecking=accept-new ubuntu@$new_ec2_instance_public_ip "
+ssh -o StrictHostKeyChecking=accept-new ubuntu@"$new_ec2_instance_public_ip" "
 	unzip ./awesome.tar.zip
 	docker load --input awesome.tar
-	docker run --detach -p 80:9999 -p 443:9999 --restart="always" awesome:`cat awesome_image_tag_name.txt`
+	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$(cat awesome_image_tag_name.txt)
 "
