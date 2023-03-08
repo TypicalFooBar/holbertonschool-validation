46149a89537813c4ebd7924e0d6d9a646c9ff137
Derek Webb <dwebb.af@gmail.com>
2023-03-08 14:18:27 -0600

    Trying to fix a bug

--- module5_task1/ensure-server-setup.sh
+++ module5_task1/ensure-server-setup.sh
@@ -7,7 +7,7 @@
 # Start Docker: sudo systemctl start docker
 # Make Docker start on boot of the system: sudo systemctl enable docker
 # Add the ubuntu user to the docker group - this will let us use ssh to communicate with docker from a client: sudo usermod -aG docker ubuntu
-ssh -o StrictHostKeyChecking=accept-new ubuntu@"$1" "
+ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=60 ubuntu@"$1" "
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install docker.io unzip -y
