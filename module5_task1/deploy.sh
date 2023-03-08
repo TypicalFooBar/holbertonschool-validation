5fa0728f7561674435bb8132c02da63c9ebfa9c7
Derek Webb <dwebb.af@gmail.com>
2023-03-08 15:37:23 -0600

    Better syntax

--- module5_task1/deploy.sh
+++ module5_task1/deploy.sh
@@ -3,10 +3,13 @@
 # Read the IP address from the file
 new_ec2_instance_public_ip=$(cat new_ec2_instance_public_ip.txt)
 
+# Read the tag name to use for the docker image
+docker_tag_name=$(cat awesome_image_tag_name.txt)
+
 scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
 
 ssh -o StrictHostKeyChecking=accept-new ubuntu@"$new_ec2_instance_public_ip" "
 	unzip ./awesome.tar.zip
 	docker load --input awesome.tar
-	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$(cat awesome_image_tag_name.txt)
+	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$docker_tag_name
 "
