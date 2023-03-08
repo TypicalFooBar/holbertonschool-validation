79c46a97c40bdcddfa1c9068117cb27f88c9a827
Derek Webb <dwebb.af@gmail.com>
2023-03-08 14:12:07 -0600

    Fixed bug in deploy script

--- module5_task1/deploy.sh
+++ module5_task1/deploy.sh
@@ -8,5 +8,5 @@ scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@$new_ec2_instan
 ssh -o StrictHostKeyChecking=accept-new ubuntu@$new_ec2_instance_public_ip "
 	unzip ./awesome.tar.zip
 	docker load --input awesome.tar
-	docker run --rm --detach -p 80:9999 -p 443:9999 --restart="always" awesome:`cat awesome_image_tag_name.txt`
+	docker run --detach -p 80:9999 -p 443:9999 --restart="always" awesome:`cat awesome_image_tag_name.txt`
 "
