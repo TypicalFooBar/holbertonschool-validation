8be80b54b0edff7735dfc3146485f95713f8fc6b
Derek Webb <dwebb.af@gmail.com>
2023-03-09 15:47:36 -0600

    Removed keys

--- module5_task1/deploy.sh
+++ module5_task1/deploy.sh
@@ -6,9 +6,9 @@ new_ec2_instance_public_ip=$(cat new_ec2_instance_public_ip.txt)
 # Read the name of the tag to use with the docker image
 dockerImageTag=$(cat awesome_image_tag_name.txt)
 
-scp -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
+scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$new_ec2_instance_public_ip":/home/ubuntu/
 
-ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem ubuntu@"$new_ec2_instance_public_ip" "
+ssh -o StrictHostKeyChecking=accept-new ubuntu@"$new_ec2_instance_public_ip" "
 	unzip ./awesome.tar.zip
 	docker load --input awesome.tar
 	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$dockerImageTag
