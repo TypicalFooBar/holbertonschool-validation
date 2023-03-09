f0f4022f77bb37766f9ca2a8ee829f115cac1c98
Derek Webb <dwebb.af@gmail.com>
2023-03-09 15:39:17 -0600

    Key test

--- module5_task1/ensure-server-setup.sh
+++ module5_task1/ensure-server-setup.sh
@@ -14,7 +14,7 @@ aws ec2 wait instance-running --instance-ids $ec2InstanceId
 # Make Docker start on boot of the system: sudo systemctl enable docker
 # Add the ubuntu user to the docker group - this will let us use ssh to communicate with docker from a client: sudo usermod -aG docker ubuntu
 
-ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=60 ubuntu@"$1" "
+ssh -o StrictHostKeyChecking=accept-new -i ~/.ssh/awesome-key.pem -o ConnectTimeout=60 ubuntu@"$1" "
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install docker.io unzip -y
